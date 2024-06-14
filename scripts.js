38d425bf725c8e3d3be3c8e68fb199ccf2bff997
Joshua Shiman <joshshiman@proton.me>
2024-06-14 12:42:18 -0400

    Update cases section

--- scripts.js
+++ scripts.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const header = document.getElementById('header');
     const heroSection = document.getElementById('hero');
+    let lastScroll = 0;
 
     function checkScroll() {
         const heroBottom = heroSection.getBoundingClientRect().bottom;
-        if (heroBottom <= 0) {
-            header.style.opacity = '0';
-        } else {
-            header.style.opacity = '1';
+        const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (currentScroll > lastScroll && heroBottom <= 0) {
+            header.classList.add('hide');
+            header.classList.remove('show');
+        } else if (currentScroll < lastScroll || heroBottom > 0) {
+            header.classList.add('show');
+            header.classList.remove('hide');
         }
+
+        lastScroll = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
     }
 
     window.addEventListener('scroll', checkScroll);
